b2adda562792c355c875977e9443f0c4ff10c53d
Xiong Tianrui <121947202+Ihavenochoised@users.noreply.github.com>
2026-06-16 11:55:37 +0000

    A bit of log cleanup

--- index.js
+++ index.js
@@ -32,4 +32,4 @@ app.use((req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`\n🚀 Server running on port ${PORT}`));
